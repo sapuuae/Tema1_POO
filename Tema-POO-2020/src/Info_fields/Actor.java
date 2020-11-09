7713aaf05bd375372b06c917a35808c562cf962b
sapuuae <andrei.sapu@yahoo.com>
2020-11-09 13:44:20 +0200

    Adaugare info actor

--- Tema-POO-2020/src/Info_fields/Actor.java
+++ Tema-POO-2020/src/Info_fields/Actor.java
@@ -1,4 +1,14 @@
 package Info_fields;
 
 public class Actor {
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    private int val = 0;
+
 }
